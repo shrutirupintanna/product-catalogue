6c3ebd8a625371d361e9b362b13465e2b068a2ad
shrutirupintanna <shrutitanna2010@gmail.com>
2023-12-22 15:05:43 +0300

    added php in html

---
+++ function.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>php functions</title>
+</head>
+<body>
+
+    <?php
+
+    function addition($a,$b) {$c=$a + $b;
+    echo "the sum is : $c";}
+
+    addition($a=30,$b=40);
+    ?>
+    
+</body>
+</html>
