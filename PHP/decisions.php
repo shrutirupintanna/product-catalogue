5d9a65f34fac971abcd5fcb99bbc12473136ddb7
shrutirupintanna <shrutitanna2010@gmail.com>
2023-12-22 15:40:21 +0300

    created new files

---
+++ PHP/decisions.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Decisions</title>
+</head>
+<body>
+    <?php
+    $d=date("D")
+    if($d=="Fri")
+    {echo "go to the mosque"}
+    else
+    {echo "enjoy your weekend"}
+    ?>
+</body>
+</html>
