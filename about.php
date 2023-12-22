09ed5a9b3b9ee8b543679e50b7b4afb0d14c44ab
shrutirupintanna <shrutitanna2010@gmail.com>
2023-12-22 14:08:08 +0300

    created new pages

--- about.php
+++ about.php
@@ -13,50 +13,42 @@
             ?>
             </div>
       <div class="row">
+            <div class="col-lg-4" style="border: 2px solid black">
+            <p style="font-size: x-large; ">we also prodive services like online orders  </p>
+            </div>
 
-      <div class="col-lg-4" style="border: 2px solid black">
-      <p style="font-size: x-large; ">we also prodive services like online orders  </p>
-      </div>
+            <div class="col-lg-4" style="border: 2px solid black">
+            <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000" class="img-fluid">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="image1.jpg" alt="image1" />
+                            </div>
 
-      <div class="col-lg-4" style="border: 2px solid black">
-      <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
-
-      <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
-            <div class="carousel-inner" alt="image1">
-                <div class="carousel-item active">
-                    <img src="image1.png ">
-                </div>
-                <div class="carousel-item>
-                <img src="image2.jpg alt="image3" >  
+                            <div class="carousel-item">
+                                <img src="image2.png" alt="image2" />
+                                
+                            </div>
+                           
+                            <div class="carousel-item">
+                                <img src="image3.jpg" alt="image4" />
+                                
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="image2.jpg" alt="image3" />                   
-                </div>
-               
-                    
-                </div>
             
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </a>
-        
-        </div>
-          
-          </div>
-          
-      </div>
+            </div>
 
-      </div>
-        </div>
-        <div class="col-lg-4" style="border: 2px solid black">
-            <p style="font-size: x-large; ">for more infomation please go to the display page </p>
-          </div>
+            <div class="col-lg-4" style="border: 2px solid black">
+                <p style="font-size: x-large; ">for more infomation please go to the display page </p>
+            </div>
         </div>
 
         <div class="row" style="border: 2px solid black">
---
+++ concatenate.php
@@ -0,0 +1,13 @@
+<?php
+
+$constantAge = 25;
+
+
+$variableAge = 30;
+
+
+$result = "Constant Age: " . $constantAge . ", Variable Age: " . $variableAge;
+echo $result;
+
+
+?>
