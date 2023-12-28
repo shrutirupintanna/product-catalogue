7d5586f77d747534769731ce4fb10c0e328a8eb2
shrutirupintanna <shrutitanna2010@gmail.com>
2023-12-28 18:52:03 +0300

    added more contact information

--- deleted.php
+++ deleted.php
@@ -151,6 +151,78 @@ About
 </html>
 
 
+
+insteadOf
+
+<html>
+    <head>
+        <title>Contact page</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css"
+    </head>
+    <body>
+<div class="container">
+ <div class="row" style="border: 2px aqua solid">
+             <?php
+            include("nav.html");
+            ?>
+</div>
+
+    <div class="row" style="border: 2px solid palevioletred">
+<div class="col-lg-6" style="border :1px solid black">
+    <h1>Your contact details</h1>
+        <form>
+            <div>
+                <label>First Name</label>
+                <input type="text" place holder="Type your first name"  name="firstName" required>
+            </div>
+    
+            <div >
+                <label>Second Name</label>
+                <input type="text"  place holder="Type your second name" name="lastName" required>
+            </div>
+    
+            <div>
+                <label >Telephone</label>
+                <input type="tel" place holder="Type your tel no." name="telephone" required>
+            </div>
+    
+            <div>
+                <label >Email</label>
+                <input type="email" place holder="Type your email"  name="email" required>
+            </div> 
+
+           <h2>Your gender</h2>
+            <label for="">Male</label>
+            <input type="checkbox" name="Male">
+
+            <label for="">Female</label>
+            <input type="checkbox" name="Female">
+
+           
+            <br />
+    
+            <button  class="btn btn-default">Submit</button>
+        </form>
+</div>
+
+        <div class="col-lg-6" style="border : 1px solid black"> 
+            <h2>Our contact details</h2> 
+ <center>  <p style="font-size: xx-large; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+            our email ; productcatalogue202045@gmail.com <br />contact;0729167215 <br />
+             for more infomation pls contact to the tel no. above</p> </center>
+           </div>
+</div>
+        
+        <div class="row" style="border: 2px solid palevioletred;">
+      <p>we provide the best quality items</p>
+        </div>
+</div>
+        
+
+       </div>
+    </body>
+</html>
+
           
        
           
--- ourinfo.php
+++ ourinfo.php
@@ -1,68 +1,88 @@
 <html>
     <head>
-        <title>Contact page</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css"
+        <title>index page.</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+      <style>
+        
+        body  {
+            background-image:screenshot.png ;
+           
+         }
+        
+      </style>
+       
     </head>
     <body>
-<div class="container">
- <div class="row" style="border: 2px aqua solid">
-             <?php
-            include("nav.html");
-            ?>
-</div>
+        <div class="container-fluid" >
+    
+            <div class="row" style="font-size: large;border: 2px solid black">
+                    <?php
+                include("nav.html");
+                ?>
+            </div>
 
-    <div class="row" style="border: 2px solid palevioletred">
-<div class="col-lg-6" style="border :1px solid black">
-    <h1>Your contact details</h1>
+            <div class="row" style="background-image:screenshot.png;padding-bottom:100px;">
+
+                <h1  style="font-size:large; ">
+                    Our contact information
+                </h1> 
+
+                <p style="font-size:x-large"><b>You can contact as through the following ;</b></p>
+               <p><b> <i>Our email</i></b>: productcatalogue0123@gmail.com<br />
+                <b><i>Our whatsapp contacts</i></b>: 071234567 , 077654321
+                </p>
+                
+            </div>
+                        <hr />
+            
+                <h2 style="font-size:x-large; ">
+                    To reach us please fill in the form below 
+                </h2>   
+                
         <form>
             <div>
-                <label>First Name</label>
+                <label><b>First Name</b></label>
                 <input type="text" place holder="Type your first name"  name="firstName" required>
             </div>
     
             <div >
-                <label>Second Name</label>
+                 <label><b>Second Name</b></label>
                 <input type="text"  place holder="Type your second name" name="lastName" required>
             </div>
     
             <div>
-                <label >Telephone</label>
+                 <label><b>Telephone</b></label>
                 <input type="tel" place holder="Type your tel no." name="telephone" required>
             </div>
     
             <div>
-                <label >Email</label>
+                 <label><b>Email</b></label>
                 <input type="email" place holder="Type your email"  name="email" required>
             </div> 
+            <br />
 
-           <h2>Your gender</h2>
+           <p><b>Your gender</b></p>
             <label for="">Male</label>
             <input type="checkbox" name="Male">
 
             <label for="">Female</label>
             <input type="checkbox" name="Female">
 
+            <br />
+            <div>
+            <label><b>Address</b></label>
+            <input type="text" place holder="type your home address" name="address" required>
+        </div>
+
            
             <br />
     
-            <button  class="btn btn-default">Submit</button>
+            <button  class="btn btn-default"><b>Submit</b></button>
         </form>
-</div>
+            </div>
 
-        <div class="col-lg-6" style="border : 1px solid black"> 
-            <h2>Our contact details</h2> 
- <center>  <p style="font-size: xx-large; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
-            our email ; productcatalogue202045@gmail.com <br />contact;0729167215 <br />
-             for more infomation pls contact to the tel no. above</p> </center>
-           </div>
-</div>
-        
-        <div class="row" style="border: 2px solid palevioletred;">
-      <p>we provide the best quality items</p>
+            
         </div>
-</div>
-        
-
-       </div>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
